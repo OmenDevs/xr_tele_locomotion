1dbec8deb469ce358fefab292a3ae6cc95aa2e94
canifty <can-dindar@outlook.com>
2026-02-18 10:47:38 +0100

    Initial Commit

---
+++ Locomotion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Locomotion
+//
+//  Created by Can Dindar on 18/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Locomotion/LocomotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocomotionApp.swift
+//  Locomotion
+//
+//  Created by Can Dindar on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LocomotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
