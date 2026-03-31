805208c4383e66e78d4ce22f35a5212721c72ae4
Juliesg02 <84135062+Juliesg02@users.noreply.github.com>
2026-03-31 12:58:39 +0200

    feat(SimulationView) Robot is presented in content as a box

--- Locomotion/View/Simulation/SimulationView.swift
+++ Locomotion/View/Simulation/SimulationView.swift
@@ -14,10 +14,28 @@ struct SimulationView: View {
     var recording: RecordingViewModel
     var body: some View {
         RealityView { content in
+            let mesh = MeshResource.generateBox(size: 0.3)
+            let material = SimpleMaterial(color: .cyan, roughness: 0.5, isMetallic: false)
+            let robot = ModelEntity(mesh: mesh, materials: [material])
+            robot.name = "robot"
+            content.add(robot)
+
             frameSubscription = content.subscribe(to: SceneEvents.Update.self) { event in
                 let deltaTime = event.deltaTime
                 simulationTick(deltaTime: deltaTime)
+
+                guard let robot = event.scene.findEntity(named: "robot") else { return }
+                                robot.position = SIMD3<Float>(
+                                    Float(robotSimulator.robotX),
+                                    0,
+                                    Float(robotSimulator.robotY)
+                                )
+                                robot.orientation = simd_quatf(
+                                    angle: Float(-robotSimulator.robotHeading),
+                                    axis: SIMD3<Float>(0, 1, 0)
+                                )
             }
+
         }
     }
     func simulationTick(deltaTime: TimeInterval) {
