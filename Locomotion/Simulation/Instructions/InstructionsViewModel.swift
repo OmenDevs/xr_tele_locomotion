6e75ce39fe5d5fbd137057268f6e878d0facccc4
Juliesg02 <84135062+Juliesg02@users.noreply.github.com>
2026-05-14 15:38:38 +0200

    Feat(Instructions): IntructionViewModel + InstructionModel saves the text and the name of the gift

---
+++ Locomotion/Simulation/Instructions/InstructionsViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  Instructions+ViewModel.swift
+//  Locomotion
+//
+//  Created by Julio Enrique Sanchez Guajardo on 14/05/26.
+//
+
+struct Instructions {
+    static let astrolavium: [Instruction] = [
+        Instruction(
+            text: "Try pinching with your middle finger and thumb to access the controls",
+            gift: nil),
+        Instruction(
+            text: "Try to move the robot, drag the ball from the starting point forward",
+            gift: nil),
+        Instruction(
+            text: "Try to move the robot, drag the ball from the starting point backward",
+            gift: nil),
+        Instruction(
+            text: "Try to move the robot, drag the ball from the starting point left or right",
+            gift: nil),
+        Instruction(
+            text: "Rotating your wrist makes the robot rotate. While pinching, try rotating your wrist to the right",
+            gift: nil),
+        Instruction(
+            text: "While pinching, try rotating your wrist to the left",
+            gift: nil),
+    ]
+    
+    static let joytick3D: [Instruction] = [
+        Instruction(
+            text: "Try pinching with your middle finger and thumb to access the controls",
+            gift: nil),
+        Instruction(
+            text: "Try to move the robot, drag the ball from the starting point forward",
+            gift: nil),
+        Instruction(
+            text: "Try to move the robot, drag the ball from the starting point backward",
+            gift: nil),
+        Instruction(
+            text: "Try to move the robot, drag the ball from the starting point left or right",
+            gift: nil),
+        Instruction(
+            text: "Rotating your wrist makes the robot rotate. While pinching, try rotating your wrist to the right",
+            gift: nil),
+        Instruction(
+            text: "While pinching, try rotating your wrist to the left",
+            gift: nil),
+    ]
+}
